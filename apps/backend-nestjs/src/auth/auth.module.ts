11cbc989a3c266d82506929ce9bb796d81367376
willtenisom <139146403+willtenisom@users.noreply.github.com>
2025-08-22 17:35:23 -0300

    chore(config): add .env and .env.example for API configuration

--- apps/backend-nestjs/src/auth/auth.module.ts
+++ apps/backend-nestjs/src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
@@ -9,9 +11,14 @@ import { AuthController } from './auth.controller';
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: 'SUA_CHAVE_SECRETA_AQUI',
-      signOptions: { expiresIn: '1h' },
+    ConfigModule, 
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRATION') || '1h' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, UsersService, JwtStrategy],
